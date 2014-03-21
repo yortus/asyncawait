b11f19eaa81f2edc984b1c43b9c5c82025424e81
Troy Gerwien <yortus@gmail.com>
2014-03-21 15:20:34 +0800

    bugfix

--- await.js
+++ await.js
@@ -1,16 +1,16 @@
 ﻿var Fiber = require('fibers');
 
 // This is the await() API function (see docs).
-// NB: No references to Fiber.current are held in the function. Holding such a
-// reference might prevent the fiber from ever being eligible for garbage collection.
 function await(expr) {
+    var fiber = Fiber.current;
+
     // Set up handlers for when expr's promise is resolved or rejected.
     expr.then(function (resolvedValue) {
         // Resume the current fiber only when expr's promise is resolved.
-        Fiber.current.run(resolvedValue);
+        fiber.run(resolvedValue);
     }, function (rejectionValue) {
         // If expr's promise is rejected, throw an exception in the fiber.
-        Fiber.current.throwInto(rejectionValue);
+        fiber.throwInto(rejectionValue);
     });
 
     // Suspend the fiber until either of the above handlers is triggered. Upon resumption,
--- src/await.ts
+++ src/await.ts
@@ -4,21 +4,20 @@ export = await;
 
 
 // This is the await() API function (see docs).
-// NB: No references to Fiber.current are held in the function. Holding such a
-// reference might prevent the fiber from ever being eligible for garbage collection.
 function await<T>(expr: AsyncAwait.Thenable<T>): T {
+    var fiber = Fiber.current;
 
     // Set up handlers for when expr's promise is resolved or rejected.
     expr.then(
         resolvedValue => {
 
             // Resume the current fiber only when expr's promise is resolved.
-            Fiber.current.run(resolvedValue);
+            fiber.run(resolvedValue);
         },
         rejectionValue => {
 
             // If expr's promise is rejected, throw an exception in the fiber.
-            Fiber.current.throwInto(rejectionValue);
+            fiber.throwInto(rejectionValue);
         }
     );
 
