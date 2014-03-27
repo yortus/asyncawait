88372930d112d28094012be4fe6f71cc292dfcdf
Troy Gerwien <yortus@gmail.com>
2014-03-28 00:56:12 +0800

    switch default benchmark

--- comparison/benchmark.js
+++ comparison/benchmark.js
@@ -17,7 +17,7 @@ var variants = {
 var LARGEST_DIR = path.join(__dirname, '.');
 var SAMPLES_PER_RUN = 1000;
 var RUNS_PER_BENCHMARK = 10;
-var SELECTED_VARIANT = variants.synchronous;
+var SELECTED_VARIANT = variants.asyncawait;
 
 
 // Run the benchmark.
