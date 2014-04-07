2bb4f0ad567073e51b76cd78b5311c3117913fab
Troy Gerwien <yortus@gmail.com>
2014-04-07 19:22:12 +0800

    await optimisations (WIP)
    no longer creates a new promise on each await

--- await.js
+++ await.js
@@ -3,7 +3,57 @@ var Promise = require('bluebird');
 var deep = require('deep');
 
 // This is the await() API function (see docs).
-var await = function (expr) {
+var await = function (expr_) {
+    // Parse argument(s). If not a single argument, treat it like an array was passed in.
+    var expr = expr_;
+    if (arguments.length !== 1) {
+        expr = new Array(arguments.length);
+        for (var i = 0; i < arguments.length; ++i)
+            expr[i] = arguments[i];
+    }
+
+    // Handle each supported 'awaitable' appropriately...
+    var fiber = Fiber.current, typeName;
+    if (!expr) {
+        // A falsy value: resume the coroutine with the value.
+        setImmediate(fiber.run, fiber, expr);
+    } else if (typeof expr.then === 'function') {
+        // A promise: resume the coroutine with the resolved value, or throw the rejection value into it.
+        expr.then(function (val) {
+            return fiber.run(val);
+        }, function (err) {
+            return fiber.throwInto(err);
+        });
+    } else if (typeof expr === 'function') {
+        // A thunk: resume the coroutine with the callback value, or throw the errback value into it.
+        expr(function (err, val) {
+            if (err)
+                fiber.throwInto(err);
+            else
+                fiber.run(val);
+        });
+    } else if ('[object Array]' === (typeName = Object.prototype.toString.call(expr))) {
+        //TODO: reword: An array: resume the coroutine with a resolved array as per Promise.all
+        //TODO: handle thunks in there too
+        Promise.all(expr).then(function (val) {
+            return fiber.run(val);
+        }, function (err) {
+            return fiber.throwInto(err);
+        });
+    } else if (typeName === '[object Object]') {
+        throw new Error('NOT IMPLEMENTED!!!!!');
+    } else {
+        // Anything else: resume the coroutine with the value.
+        setImmediate(fiber.run, fiber, expr);
+    }
+
+    // Suspend the current fiber until the one of the above handlers runs it again.
+    // TODO: reword:  When the fiber is resumed, return the resolved value of expr's promise.
+    return Fiber.yield();
+};
+
+//TODO: Remove once the plain object case has been properly transferred to above code
+var awaitOld = function (expr) {
     // Set up promise settlement handlers that either resume, or throw into, the current fiber.
     var fiber = Fiber.current;
     function onResolved(val) {
@@ -40,9 +90,26 @@ function reduceToPromise(expr) {
     if (isThunk)
         return thunkToPromise(expr);
 
-    // A plain object or array - return a promise of a deep clone with all contained promises resolved.
+    // An array - return a promise of the same array instance with all contained promises replaced by their resolution values.
     var typeName = Object.prototype.toString.call(expr);
-    if (typeName === '[object Object]' || typeName === '[object Array]') {
+    if (typeName === '[object Array]') {
+        // Make a promise that's resolved when all the array's thunks and promises are resolved.
+        //var prom = Promise.resolve(expr);
+        //for (var i = 0; i < expr.length; ++i) {
+        //    var elem = expr[i];
+        //    if (!elem) continue;
+        //    if (typeof elem.then === 'function') {
+        //        prom = prom.then(() => elem.then(expr)); // Promise
+        //    } else if (typeof elem === 'function') {
+        //        prom = prom.then(() => thunkToPromise(elem).then(expr)); // Thunk
+        //    }
+        //}
+        var prom = Promise.all(expr);
+        return prom;
+    }
+
+    // A plain object - return a promise of a deep clone with all contained promises resolved.
+    if (typeName === '[object Object]') {
         // Clone the original object so we don't modify it, converting thunks to promises along the way.
         var clone = deep.transform(expr, function (obj) {
             return typeof obj === 'function';
--- src/await.ts
+++ src/await.ts
@@ -6,7 +6,80 @@ export = await;
 
 
 // This is the await() API function (see docs).
-var await: AsyncAwait.IAwait = function(expr) {
+var await: AsyncAwait.IAwait = function(expr_) {
+
+    // Parse argument(s). If not a single argument, treat it like an array was passed in.
+    var expr = expr_;
+    if (arguments.length !== 1) {
+        expr = new Array(arguments.length);
+        for (var i = 0; i < arguments.length; ++i) expr[i] = arguments[i];
+    }
+
+    // Handle each supported 'awaitable' appropriately...
+    var fiber = Fiber.current, typeName;
+    if (!expr) {
+
+        // A falsy value: resume the coroutine with the value.
+        setImmediate(fiber.run, fiber, expr);
+    }
+    else if (typeof expr.then === 'function') {
+
+        // A promise: resume the coroutine with the resolved value, or throw the rejection value into it.
+        expr.then(val => fiber.run(val), err => fiber.throwInto(err));
+    }
+    else if (typeof expr === 'function') {
+
+        // A thunk: resume the coroutine with the callback value, or throw the errback value into it.
+        expr((err, val) => { if (err) fiber.throwInto(err); else fiber.run(val); });
+    }
+    else if ('[object Array]' === (typeName = Object.prototype.toString.call(expr))) {
+
+        //TODO: reword: An array: resume the coroutine with a resolved array as per Promise.all
+        //TODO: handle thunks in there too
+        Promise.all(expr).then(val => fiber.run(val), err => fiber.throwInto(err));
+    }
+    else if (typeName === '[object Object]') {
+
+        //TODO: A plain object...
+        throw new Error('NOT IMPLEMENTED!!!!!');
+    }
+    else {
+
+        // Anything else: resume the coroutine with the value.
+        setImmediate(fiber.run, fiber, expr);
+    }
+
+    // Suspend the current fiber until the one of the above handlers runs it again.
+    // TODO: reword:  When the fiber is resumed, return the resolved value of expr's promise.
+    return Fiber.yield();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO: Remove once the plain object case has been properly transferred to above code
+var awaitOld: AsyncAwait.IAwait = function(expr) {
 
     // Set up promise settlement handlers that either resume, or throw into, the current fiber.
     var fiber = Fiber.current;
@@ -38,9 +111,27 @@ function reduceToPromise(expr) {
     var isThunk = typeof expr === 'function';
     if (isThunk) return thunkToPromise(expr);
 
-    // A plain object or array - return a promise of a deep clone with all contained promises resolved.
+    // An array - return a promise of the same array instance with all contained promises replaced by their resolution values.
     var typeName = Object.prototype.toString.call(expr);
-    if (typeName === '[object Object]' || typeName === '[object Array]') {
+    if (typeName === '[object Array]') {
+
+        // Make a promise that's resolved when all the array's thunks and promises are resolved.
+        //var prom = Promise.resolve(expr);
+        //for (var i = 0; i < expr.length; ++i) {
+        //    var elem = expr[i];
+        //    if (!elem) continue;
+        //    if (typeof elem.then === 'function') {
+        //        prom = prom.then(() => elem.then(expr)); // Promise
+        //    } else if (typeof elem === 'function') {
+        //        prom = prom.then(() => thunkToPromise(elem).then(expr)); // Thunk
+        //    }
+        //}
+        var prom = Promise.all(expr); //TODO: thunks not handled
+        return prom;
+    }
+
+    // A plain object - return a promise of a deep clone with all contained promises resolved.
+    if (typeName === '[object Object]') {
 
         // Clone the original object so we don't modify it, converting thunks to promises along the way.
         var clone = deep.transform(expr, obj => typeof obj === 'function', thunkToPromise);
