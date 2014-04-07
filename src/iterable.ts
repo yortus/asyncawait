0c84a19da963cf077a45229dce4ae87b5e2d7d8d
Troy Gerwien <yortus@gmail.com>
2014-04-07 19:51:17 +0800

    more stubbing

--- src/iterable.ts
+++ src/iterable.ts
@@ -1,1 +1,25 @@
-﻿
+﻿import _refs = require('_refs');
+import Fiber = require('fibers');
+import Promise = require('bluebird');
+export = iterable;
+
+
+// This is the iterable() API function (see docs).
+var iterable = function(fn: Function) {
+
+    //TODO:...
+    return function () {
+
+        // Capture initial arguments used to start the iterator
+        var initArgs = new Array(arguments.length);
+        for (var i = 0; i < initArgs.length; ++i) initArgs[i] = arguments[i];
+
+        //TODO...
+        return {
+            next: function() {
+
+
+            }
+        };
+    };
+};
--- src/yield.ts
+++ src/yield.ts
@@ -1,1 +1,12 @@
-﻿
+﻿import _refs = require('_refs');
+import Fiber = require('fibers');
+import Promise = require('bluebird');
+export = yield_;
+
+
+// This is the yield() API function (see docs).
+var yield_ = function(expr) {
+
+
+
+}
