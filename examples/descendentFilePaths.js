b6a5bfd5841f89c06d4531d1c2fc0564f69a6bf8
yortus <yortus@gmail.com>
2014-04-08 14:25:39 +0800

    working fs tree walking example

---
+++ examples/descendentFilePaths.js
@@ -0,0 +1,45 @@
+﻿var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var path = require('path');
+var _ = require('lodash');
+var async = require('../async');
+var await = require('../await');
+var iterable = require('../iterable');
+var yield_ = require('../yield');
+
+
+var descendentFilePaths = iterable (function recurse(dir, recursive) {
+
+    var files = await (fs.readdirSync(dir));
+    var paths = _.map(files, function (file) { return path.join(dir, file); });
+    var stats = await (_.map(paths, function (path) { return fs.statAsync(path); }));
+
+    _.each(stats, function(stat, i) {
+        if (stat.isFile()) yield_ (paths[i]);
+        else if (recursive) recurse(paths[i], true);
+    });
+});
+
+
+var program = async (function(dir) {
+    var paths = descendentFilePaths(dir, true);
+
+    while (true) {
+        var path = paths.next();
+        if (await(path.done)) break;
+        console.log(await(path.value));
+    }
+
+    return 'Finished!';
+});
+
+
+console.log('running...');
+program(path.join(__dirname, '.'))
+    .then(function (result) {
+        console.log(result);
+    })
+    .catch(function(err) {
+        console.log('----- rejected: -----');
+        console.log(err);
+    });
