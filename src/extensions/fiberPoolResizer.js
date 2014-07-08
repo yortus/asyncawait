ff56c3ddd775022433e5e45d5011752ae64e2155
Troy Gerwien <yortus@gmail.com>
2014-07-08 23:27:11 +0800

    ...contd

--- src/extensions/fiberPoolResizer.js
+++ src/extensions/fiberPoolResizer.js
@@ -1,17 +1,38 @@
-﻿var fiberPool = require('../fiberPool');
+﻿var Fiber = require('../fibers');
 
 
+//TODO: doc...
 var extension = function (pipeline) {
     return ({
         acquireFiber: function (body) {
-            fiberPool.inc();
+            inc();
             return pipeline.acquireFiber(body);
         },
         releaseFiber: function (fiber) {
-            fiberPool.dec();
+            dec();
             return pipeline.releaseFiber(fiber);
         }
     });
 };
+
+// The following functionality prevents memory leaks in node-fibers
+// by actively managing Fiber.poolSize. For more information, see
+// https://github.com/laverdet/node-fibers/issues/169.
+/** Increment the number of active fibers. */
+function inc() {
+    ++_activeFiberCount;
+    if (_activeFiberCount >= _fiberPoolSize) {
+        _fiberPoolSize += 100;
+        Fiber.poolSize = _fiberPoolSize;
+    }
+}
+
+/** Decrement the number of active fibers. */
+function dec() {
+    --_activeFiberCount;
+}
+
+var _fiberPoolSize = Fiber.poolSize;
+var _activeFiberCount = 0;
 module.exports = extension;
 //# sourceMappingURL=fiberPoolResizer.js.map
--- src/extensions/fiberPoolResizer.ts
+++ src/extensions/fiberPoolResizer.ts
@@ -1,16 +1,42 @@
 ﻿import references = require('references');
-import fiberPool = require('../fiberPool'); //TODO: move fiberPool code into here?
+import Fiber = require('../fibers');
 import Extension = AsyncAwait.Extension;
 export = extension;
 
 
+//TODO: doc...
 var extension: Extension = (pipeline) => ({
     acquireFiber: body => {
-        fiberPool.inc();
+        inc();
         return pipeline.acquireFiber(body);
     },
     releaseFiber: fiber => {
-        fiberPool.dec();
+        dec();
         return pipeline.releaseFiber(fiber);
     }
 });
+
+
+// The following functionality prevents memory leaks in node-fibers
+// by actively managing Fiber.poolSize. For more information, see
+// https://github.com/laverdet/node-fibers/issues/169.
+
+
+/** Increment the number of active fibers. */
+function inc() {
+    ++_activeFiberCount;
+    if (_activeFiberCount >= _fiberPoolSize) {
+        _fiberPoolSize += 100;
+        Fiber.poolSize = _fiberPoolSize;
+    }
+}
+
+
+/** Decrement the number of active fibers. */
+function dec() {
+    --_activeFiberCount;
+}
+
+
+var _fiberPoolSize = Fiber.poolSize;
+var _activeFiberCount = 0;
