f77f40248ca9ff81c16d739e325d76a63dca4214
Troy Gerwien <yortus@gmail.com>
2014-07-17 18:04:59 +0800

    add profiler example

---
+++ support/examples/profiler.js
@@ -0,0 +1,23 @@
+﻿var fs = require('fs');
+var Promise = require('bluebird');
+var async = require('../../async');
+var await = require('../../await');
+var use = require('../../src/use');
+var maxConcurrency = require('../../mods/maxConcurrency');
+use(maxConcurrency(1));
+
+
+var program = async (function prog() {
+    return 1;
+});
+
+
+var promises = [];
+for (var i = 0; i < 10000; ++i) promises.push(program());
+Promise.all(promises)
+.then(function (results) {
+    console.log('results: ' + results.length);
+})
+.catch(function (err) {
+    console.log('error: ' + err);
+});
