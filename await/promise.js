24ee845ff624908ad528f1db191f016dd2c8706d
Troy Gerwien <yortus@gmail.com>
2014-07-21 08:44:20 +0800

    added TODO comments

--- await/promise.js
+++ await/promise.js
@@ -11,5 +11,25 @@ var builder = oldBuilder.derive(function () {
         }, co.enter);
     };
 });
+
+//TODO: possible new style... could be similar for async builder and global mods
+//TODO: call with await.make(mod), async.make(mod), config.use(mod)
+//TODO: but overrideHandler call needs (REALLY??? check) to happen *after* user has a chance to set options
+//      with config(...). So, builders must call the override...() func lazily ie when first
+//      async(...) or await(...) call is made.
+var builder2 = oldBuilder.derive({
+    name: 'promise',
+    type: null,
+    overrideHandler: function (baseHandler, options) {
+        return function promiseHandler(co, arg, allArgs) {
+            if (allArgs || !_.isPromise(arg))
+                return pipeline.notHandled;
+            arg.then(function (val) {
+                return co.enter(null, val);
+            }, co.enter);
+        };
+    },
+    defaultOptions: {}
+});
 module.exports = builder;
 //# sourceMappingURL=promise.js.map
--- await/promise.ts
+++ await/promise.ts
@@ -11,3 +11,25 @@ var builder = oldBuilder.derive<AsyncAwait.Await.PromiseBuilder>(
         arg.then(val => co.enter(null, val), co.enter);
     }
 );
+
+
+
+
+//TODO: possible new style... could be similar for async builder and global mods
+//TODO: call with await.make(mod), async.make(mod), config.use(mod)
+//TODO: but overrideHandler call needs (REALLY??? check) to happen *after* user has a chance to set options
+//      with config(...). So, builders must call the override...() func lazily ie when first
+//      async(...) or await(...) call is made.
+var builder2 = oldBuilder.derive<AsyncAwait.Await.PromiseBuilder>({
+
+    name: 'promise', //TODO: this could be used for automatic type deduction, if it works
+
+    type: <AsyncAwait.Await.PromiseBuilder> null,
+
+    overrideHandler: (baseHandler, options) => function promiseHandler(co, arg, allArgs) {
+        if (allArgs || !_.isPromise(arg)) return pipeline.notHandled;
+        arg.then(val => co.enter(null, val), co.enter);
+    },
+
+    defaultOptions: {}
+});
