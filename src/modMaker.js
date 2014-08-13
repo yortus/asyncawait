eedb57b85ffc08eff43d98e9668035180e12169a
yortus <yortus@gmail.com>
2014-08-13 10:23:26 +0800

    removed modMaker

--- src/modMaker.js
+++
@@ -1,9 +0,0 @@
-﻿//TODO: put this in util?
-
-function modMaker(oldState, modList, newMod) {
-    var isOptionsOnly = !newMod.overrideProtocol;
-
-    //var overrideProtocol = isOptionsOnly ? old.
-    return null;
-}
-//# sourceMappingURL=modMaker.js.map
